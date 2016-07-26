a6401b5a2d2ef6c0de3276f61ac7cb1260566721
yanghaitao <13021003665@163.com>
2016-07-26 20:16:06 +0800

    Initial Commit

---
+++ JingJing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JingJing
+//
+//  Created by yanghaitao  on 16/7/26.
+//  Copyright © 2016年 yanghaitao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
